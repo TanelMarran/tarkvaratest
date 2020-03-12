69f87fa779de44a90da70dd266f0e2673f6711dc
TanelMarran <marran.tanel@gmail.com>
2020-03-12 17:01:58 +0200

    Added 6 test for IC

--- Kodutoo4/NextDate-2/src/test/java/ee/ut/cs/swt/nextdate/NextDateTest.java
+++ Kodutoo4/NextDate-2/src/test/java/ee/ut/cs/swt/nextdate/NextDateTest.java
@@ -21,7 +21,7 @@ public class NextDateTest {
 	public final void testJuneToJuly(){
 	assertEquals("7/1/2001", trialDate.run(6, 30, 2001));
 	}
-	
+
 	@Test
 	public final void testMarchToApril(){
 	assertEquals("4/1/2001", trialDate.run(3, 31, 2001));
@@ -32,5 +32,39 @@ public class NextDateTest {
 	assertEquals("1/1/2002", trialDate.run(12, 31, 2001));
 	}
 
+	@Test //1
+	public final void testFebuaryToMarch() {
+		assertEquals("3/1/2020", trialDate.run(2, 29, 2020));
+	}
+
+	@Test //2
+	public final void testFebuaryToMarchNotLeapYear(){
+		assertEquals("3/1/2021", trialDate.run(2, 28, 2021));
+	}
+
+	@Test //3
+	public final void testNovember(){
+		assertEquals("11/15/2021", trialDate.run(11, 14, 2021));
+	}
+
+	@Test //4
+	public final void testFebuary(){
+		assertEquals("2/15/2021", trialDate.run(2, 14, 2021));
+	}
+
+	@Test //5
+	public final void testJuly(){
+		assertEquals("7/7/2020", trialDate.run(7, 6, 2020));
+	}
+
+	@Test //6
+	public final void testFebuaryLeapYear(){
+		assertEquals("2/29/2020", trialDate.run(2, 28, 2020));
+	}
+
+	@Test //7
+	public final void test(){
+		assertEquals("2/29/2020", trialDate.);
+	}
 }
 
