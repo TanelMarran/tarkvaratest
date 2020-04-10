f15e6b093c67737bf4051cee83670e07b2db20b4
TanelMarran <marran.tanel@gmail.com>
2020-04-10 13:06:25 +0300

    Finished tests for Calculator tab

--- Kodutoo8/SikuliX Project/homework_solution.sikuli/homework_solution.py
+++ Kodutoo8/SikuliX Project/homework_solution.sikuli/homework_solution.py
@@ -25,7 +25,7 @@ class Folders(unittest.TestCase):
 
     # Dragging an icon to the folder makes it disappear from the screen.
     # [“test_apps_delete”]
-    def _test_apps_delete(self):
+    def test_apps_delete(self):
         app_image = "app_image.png"
         blue_folder = "blue_folder.png"
         number_of_apps = len(findAllList(app_image))
@@ -37,7 +37,7 @@ class Folders(unittest.TestCase):
 
     # Dragging a folder over a folder doesn’t make anything disappear.
     # Dragged folder should appear to be on top of other folder. [“test_move_folders”]
-    def _test_move_folders(self):
+    def test_move_folders(self):
         blue_folder = Pattern("blue_folder.png").similar(0.75)
         orange_folder = Pattern("orange_folder.png").similar(0.91)
         green_folder = Pattern("green_folder.png").similar(0.91)
@@ -78,10 +78,68 @@ class Folders(unittest.TestCase):
                 self.assertEquals(len(app_region.findAllList("1586509423568.png")),1)
                 mouseMove("1586507956362.png")
                 click()
-     
+
+class Calculator(unittest.TestCase):
+
+    # At first we create an instance of helperClass. 
+    h = Helper("Homework")  # name of the jar
+    
+    def setUp(self):
+        self.h.openSUT()
+        click("1586510426740.png")
+        time.sleep(0.2)
+
+    def tearDown(self):
+        self.h.closeSUT()
+
+    # Black output rectangle must be empty when nothing was typed. When
+    # something was typed, the rectangle must show everything that was typed.
+    # The rectangle must display 20 characters at the most. [“test_calc_black”]
+    def test_calc_black(self):
+        calculator = Pattern("calculator.png").targetOffset(-88,-3)
+        invalid_result = "invalid_result.png"
+        for i in range(21):
+            click(calculator)
+        assert exists(invalid_result) == None
+
+    #Purple output rectangle must show “0” when nothing was typed. When
+    #something is typed, the rectangle must output only the answer (e.g. typing
+    #“2+2” outputs “4”). When expression is not full (e.g. “2*3+”) rectangle must
+    #output the last answer (“6” in this case). [“test_calc_purple”]
+    def test_calc_purple(self):
+        purple = "purple.png"
+        delete = Pattern("calculator.png").targetOffset(0,102)
+        one = Pattern("calculator.png").targetOffset(-81,-1)
+        eight = Pattern("calculator.png").targetOffset(-30,-102)
+        plus = Pattern("calculator.png").targetOffset(86,48)
+        divide = Pattern("calculator.png").targetOffset(86,-96)
+       
+        self.assertEqual('0',findAllList(purple)[0].collectLinesText()[0])
+        click(eight)
+        click(plus)
+        self.assertEqual('8',findAllList(purple)[0].collectLinesText()[0])
+        click(one)
+        self.assertEqual('9',findAllList(purple)[0].collectLinesText()[0])
+        click(divide)
+        click(eight)
+        self.assertEqual('8.125',findAllList(purple)[0].collectLinesText()[0])
+        for i in range(5):
+            click(delete)
+        self.assertEqual('0',findAllList(purple)[0].collectLinesText()[0])
+        
+    # When typing an expression that is mathematically impossible, the purple
+    # output rectangle must show “Error”. [“test_calc_err”]
+    def test_calc_err(self):
+        purple = "purple.png"
+        click(Pattern("calculator.png").targetOffset(-28,-55))
+        click(Pattern("calculator.png").targetOffset(82,-98))
+        click(Pattern("calculator.png").targetOffset(-42,46))
+        self.assertEqual('Error',findAllList(purple)[0].collectLinesText()[0])
+        
+
 if __name__ == '__main__':
     #suite = unittest.TestLoader().loadTestsFromModule(sys.modules[__name__])
-    suite = unittest.TestLoader().loadTestsFromTestCase(Folders) 
+    suite = unittest.TestLoader().loadTestsFromTestCase(Calculator) 
     unittest.TextTestRunner().run(suite) #verbosity=3
     #reporter = createReporter(NAME)
     #reporter.run(suite)
